5e0931cad2c63389bd5e92d9b7972047c52409a9
Yaoisss <3136586731@qq.com>
2022-07-12 19:40:51 +0800

    update

--- main/main.go
+++ main/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"fusu"
+	"fusu/cmd"
+	_ "fusu/cmd"
+)
+
+var (
+	version = flag.Bool("version", false, "Show current version of Fusu.")
+)
+
+func printVersion() {
+	version := fusu.VersionStatement()
+	for _, info := range version {
+		fmt.Println(info)
+	}
+}
 
 func main() {
-	fmt.Println("hello people!")
+	flag.Parse()
+	printVersion()
+	//	获取配置文件
+	config := cmd.GetConfigFilePath()
+	d, err := cmd.LoadConfig(config)
+	if err != nil {
+		fmt.Println("failed to load config:", config)
+	}
+	fmt.Println(d)
 }
